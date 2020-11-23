b338ce28ca4bf2f86d2a13a0a3a32cba5b75bf09
ShintaroSakai <s.sakai@buysell-technologies.com>
2020-11-23 19:44:40 +0900

    Initial Commit

---
+++ sweets_serch_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sweets_serch_app
+//
+//  Created by 酒井 慎太郎 on 2020/11/15.
+//  Copyright © 2020 酒井 慎太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
